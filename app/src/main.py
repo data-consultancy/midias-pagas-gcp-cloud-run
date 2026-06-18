301e70cccb239c7f77650dd34a52423af547bd5c
consultoria-analise-dados <dataanalysis_2025@outlook.com>
2026-06-18 15:58:01 -0300

    init: estrutura base do paid-media-job

---
+++ app/src/main.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logger.info("Iniciando paid-media-job")
+    # TODO: implementar consumo de dados de mídias pagas
+    logger.info("Job finalizado com sucesso")
+
+
+if __name__ == "__main__":
+    main()
